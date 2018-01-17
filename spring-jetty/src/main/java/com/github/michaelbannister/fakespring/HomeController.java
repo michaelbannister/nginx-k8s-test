0eaa66584cef088540bb9e9d3f702a7ad4c793cb
Michael Bannister <michael.bannister@johnlewis.co.uk>
2018-01-17 13:53:50 +0000

    add customisable delay to spring app

--- spring-jetty/src/main/java/com/github/michaelbannister/fakespring/HomeController.java
+++ spring-jetty/src/main/java/com/github/michaelbannister/fakespring/HomeController.java
@@ -1,12 +1,37 @@
 package com.github.michaelbannister.fakespring;
 
+import org.apache.commons.lang3.RandomUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Random;
+
 @Controller
 public class HomeController {
+
+    @Value("${delay.min.millis:0}")
+    private long minDelayMillis;
+    @Value("${delay.max.millis:0}")
+    private long maxDelayMillis;
+
+    private Random random = new Random();
+
     @GetMapping("/")
     public String home() {
+        if (maxDelayMillis != 0) {
+            sleep(RandomUtils.nextLong(minDelayMillis, maxDelayMillis));
+        }
+
         return "index";
     }
+
+    private static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            // ignore
+        }
+    }
+
 }
